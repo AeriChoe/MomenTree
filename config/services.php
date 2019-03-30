d644d6fa8eba890e3223a2ba8d815482e46ed5ff
aeri <ehstos2@gmail.com>
2019-03-30 23:19:51 +0900

    change all3

--- config/services.php
+++ config/services.php
@@ -47,7 +47,7 @@ return [
     'github' => [
     'client_id' => env('GITHUB_CLIENT_ID'),
     'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => env('GITHUB_CALLBACK'),
+    'redirect' => 'https://whisperaeriapp.herokuapp.com/login/github/callback',
 ],
 
 ];
