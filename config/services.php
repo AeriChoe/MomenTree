31c7647ce7ae94b34d6271c2742763da94f4c66a
aeri <ehstos2@gmail.com>
2019-03-30 21:46:27 +0900

    chang set4

--- config/services.php
+++ config/services.php
@@ -47,7 +47,7 @@ return [
     'github' => [
     'client_id' => env('GITHUB_CLIENT_ID'),
     'client_secret' => env('GITHUB_CLIENT_SECRET'),
-    'redirect' => 'http://whisperaeriapp.herokuapp.com/login/github/callback',
+    'redirect' => 'whisperaeriapp.herokuapp.com/login/github/callback',
 ],
 
 ];
