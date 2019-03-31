a7046f9113c7e8af48ebf9df7d363bfbae22e1d6
aeri <ehstos2@gmail.com>
2019-03-31 09:24:47 +0900

    ch4

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
     }
     public function redirectToProvider3()
     {
-        return Socialite::driver('gg')->redirect();
+        return Socialite::driver('google')->redirect();
     }
 
 
